05b7c8c631fd0b491cefb400cd51cd6869e6e28a
zhaosj1991 <zhaosj_1991@163.com>
2021-10-01 22:40:50 +0800

    update.

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module yuan-api
 
 go 1.13
 
-require go.uber.org/zap v1.19.1
+require (
+	github.com/gin-gonic/gin v1.7.4
+	go.uber.org/zap v1.19.1
+	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.27.1
+)
--- user-web/main.go
+++ user-web/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"yuan-api/user-web/initialize"
+)
+
+func main() {
+
+	//初始化logger
+	initialize.InitLogger()
+
+	//初始化routers
+	Router := initialize.Routers()
+
+	port := 8021
+
+	zap.S().Infof("启动服务器, 端口: %d", port)
+
+	if err := Router.Run(fmt.Sprintf(":%d", port)); err != nil {
+		zap.S().Panicf("启动失败, err: %s", err.Error())
+	}
+
+}
